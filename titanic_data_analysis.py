63229a69dc5d564cdeb38979d7648d677b972150
aiitx <aiitxcom@163.com>
2020-08-07 01:09:03 +0800

    测试数据同样预处理，做预测得出结果。

--- titanic_data_analysis.py
+++ titanic_data_analysis.py
@@ -12,6 +12,7 @@ from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestRegressor
+import sklearn.preprocessing as preprocessing
 
 data_train = pd.read_csv("D:/pycharmpro_20192/python_project/titanic_20200806/titanic/train.csv")
 
@@ -195,3 +196,64 @@ dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix='Pclass')
 df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
 df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
 df
+
+# 收敛变化幅度大的数据
+scaler = preprocessing.StandardScaler()
+age_scale_param = scaler.fit(df[['Age']])
+df['Age_scaled'] = scaler.fit_transform(df[['Age']], age_scale_param)
+fare_scale_param = scaler.fit(df[['Fare']])
+df['Fare_scaled'] = scaler.fit_transform(df[['Fare']], fare_scale_param)
+df
+
+
+# 逻辑回归建模
+from sklearn import linear_model
+
+# 用正则取出我们要的属性值
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.iloc[:,:].values
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', solver='liblinear', tol=1e-6)
+clf.fit(X, y)
+
+clf
+
+
+# test.csv也和train.csv同样的预处理
+data_test = pd.read_csv("D:/pycharmpro_20192/python_project/titanic_20200806/titanic/test.csv")
+data_test.loc[(data_test.Fare.isnull()), 'Fare'] = 0
+# 接着我们对test_data做和train_data中一致的特征变换
+# 首先用同样的RandomForestRegressor模型填上丢失的年龄
+tmp_df = data_test[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].iloc[:,:].values
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[(data_test.Age.isnull()), 'Age'] = predictedAges
+
+data_test = set_cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix='Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix='Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix='Pclass')
+
+
+df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(df_test[['Age']], age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(df_test[['Fare']], fare_scale_param)
+df_test
+
+# 做预测取结果
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'PassengerId': data_test['PassengerId'], 'Survived': predictions.astype(np.int32)})
+result.to_csv("D:/pycharmpro_20192/python_project/titanic_20200806/titanic/logistic_regression_predictions.csv", index=False)
+result

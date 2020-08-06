00a4aa1d14694a6d9f1f4bbfd1f3a78f6df9bdab
aiitx <aiitxcom@163.com>
2020-08-06 23:47:28 +0800

    简单数据预处理-数据拟合，类目型特征因子化

--- titanic_data_analysis.py
+++ titanic_data_analysis.py
@@ -142,4 +142,56 @@ plt.xlabel(u"Cabin")
 plt.ylabel(u"num of people")
 plt.show()
 
+# 简单数据预处理
+# 使用 RandomForestClassifier 填补缺失的年龄属性
 
+
+def set_missing_ages(df):
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].iloc[:,:].values
+    unknown_age = age_df[age_df.Age.isnull()].iloc[:,:].values
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+
+    # 用得到的模型进行未知年龄结果预测
+    predictedages = rfr.predict(unknown_age[:, 1::])
+
+    # 用得到的预测结果填补原缺失数据
+    df.loc[(df.Age.isnull()), 'Age'] = predictedages
+
+    return df, rfr
+
+
+def set_cabin_type(df):
+    df.loc[(df.Cabin.notnull()), 'Cabin'] = "Yes"
+    df.loc[(df.Cabin.isnull()), 'Cabin'] = "No"
+    return df
+
+
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_cabin_type(data_train)
+print(data_train)
+
+# 对类目型的特征因子化
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix='Cabin')
+
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix='Embarked')
+
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix='Sex')
+
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix='Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df
